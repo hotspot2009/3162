a10efb5ef626f549f1ea733e99e010f059b6f1e9
Niaz Khan <hotspot2009@gmail.com>
2019-01-03 14:51:58 +0800

    Fixed Web Dev Issues

--- routes.py
+++ routes.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/search")
-def search():
-    return render_template("search.html")
+@app.route("/homepage", methods=['GET', 'POST'])
+def homepage():
+    if request.method == "POST":
+        input_search_term = request.form.get("input-search-term")
+        input_location = request.form.get("input-location")
+        return render_template('search.html', search_term = input_search_term, location_area = input_location)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
+    app.run(debug=True, port=1000)
