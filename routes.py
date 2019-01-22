d931783fc4fcaed87ef0efff83be05fcaedb5058
Niaz Khan <hotspot2009@gmail.com>
2019-01-23 00:41:10 +0800

    Timeline Update

--- routes.py
+++ routes.py
@@ -7,15 +7,11 @@ infile = open("codes/Stored Data/sample_aspect_info.pickle", "rb")
 aspect_info = pk.load(infile)
 infile.close()
 
-app = Flask(__name__)
+infile = open("codes/Stored Data/sample_hotel_info.pickle", "rb")
+hotels = pk.load(infile)
+infile.close()
 
-hotels=[
-    ["Hotel Arena"," s Gravesandestraat 55 Oost 1092 AA Amsterdam Netherlands", 52.3605759, 4.9159683],
-    ["K K Hotel George","1 15 Templeton Place Earl s Court Kensington and Chelsea London SW5 9NB United Kingdom", 51.4918878, -0.1949706],
-    ["Apex Temple Court Hotel","1 2 Serjeant s Inn Fleet Street City of London London EC4Y 1LL United Kingdom", 51.5137335, -0.1087512],
-    ["The Park Grand London Paddington","1 3 Queens Garden Westminster Borough London W2 3BA United Kingdom", 51.5142184, -0.1809032],
-    ["The Principal London","1 8 Russell Square Camden London WC1B 5BE United Kingdom", 51.5226217, -0.1251602]
-]
+app = Flask(__name__)
 
 @app.route("/")
 def index():
